1a7bc5ae32b184844609b9d04de5967833c4a0d2
varunnaik <varun.naik@gmail.com>
2015-01-25 16:34:37 +0530

    Spawn waves

---
+++ SpaceShooter/Assets/Scripts/GameController.js
@@ -0,0 +1,24 @@
+﻿var hazard : GameObject;
+var spawnValues : Vector3;
+var hazardCount : int;
+var spawnWait : float;
+var startWait : float;
+var waveWait : float;
+
+function Start () {
+    SpawnWaves ();
+}
+
+function SpawnWaves () {
+    yield WaitForSeconds (startWait);
+    while (true)
+    {
+        for ( var i : int= 0; i < hazardCount; i++) {
+            var spawnPosition : Vector3= new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+            var spawnRotation : Quaternion= Quaternion.identity;
+            Instantiate (hazard, spawnPosition, spawnRotation);
+            yield WaitForSeconds (spawnWait);
+        }
+        yield WaitForSeconds (waveWait);
+    }
+}
