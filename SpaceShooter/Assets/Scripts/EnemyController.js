7b0a9d28f686bc68e7b3d9c6de33d28d816b3394
varunnaik <varun.naik@gmail.com>
2015-02-02 11:47:12 +0530

    Enemy players now fire.

---
+++ SpaceShooter/Assets/Scripts/EnemyController.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var shot : GameObject;
+var shotSpawn : Transform;
+var nextFire : float;
+
+function Start () {
+	StartShooting ();
+}
+
+function StartShooting () {
+	while (true)
+    {
+		Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		audio.Play();
+        yield WaitForSeconds (nextFire);
+    }
+	
+}
